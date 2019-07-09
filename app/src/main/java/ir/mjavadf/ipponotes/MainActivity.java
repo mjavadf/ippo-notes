38cc219fcc200ae71299ea0af83092fa007ba58e
mjavadf <javad.me1376@gmail.com>
2019-07-10 00:15:35 +0430

    Modify MainActivity and init recyclerView and fab

--- app/src/main/java/ir/mjavadf/ipponotes/MainActivity.java
+++ app/src/main/java/ir/mjavadf/ipponotes/MainActivity.java
@@ -1,13 +1,26 @@
 package ir.mjavadf.ipponotes;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+  RecyclerView recyclerView;
+  FloatingActionButton addNote;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    getSupportActionBar().hide();
+
+    initViews();
+  }
+
+  private void initViews() {
+    recyclerView = findViewById(R.id.recyclerView);
+    addNote = findViewById(R.id.addNote);
   }
 }
