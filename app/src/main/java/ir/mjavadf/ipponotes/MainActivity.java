6f8054cb90ea12d1c03e4d074e73d02a1b8a5330
mjavadf <javad.me1376@gmail.com>
2019-07-11 02:10:50 +0430

    Some changes in main activity :)

--- app/src/main/java/ir/mjavadf/ipponotes/MainActivity.java
+++ app/src/main/java/ir/mjavadf/ipponotes/MainActivity.java
@@ -3,24 +3,38 @@ package ir.mjavadf.ipponotes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import ir.mjavadf.ipponotes.adapters.NotesAdapter;
+import ir.mjavadf.ipponotes.objects.Note;
 
 public class MainActivity extends AppCompatActivity {
 
   RecyclerView recyclerView;
   FloatingActionButton addNote;
+  NotesAdapter adapter;
+  List<Note> noteList = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    getSupportActionBar().hide();
+    Objects.requireNonNull(getSupportActionBar()).hide();
 
     initViews();
   }
 
   private void initViews() {
-    recyclerView = findViewById(R.id.recyclerView);
     addNote = findViewById(R.id.addNote);
+    recyclerView = findViewById(R.id.recyclerView);
+
+    adapter = new NotesAdapter(noteList);
+    recyclerView.setAdapter(adapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
   }
 }
