73eef624018f5f468ac6ce520a242dc034c033cf
mjavadf <javad.me1376@gmail.com>
2019-07-12 00:15:10 +0430

    Design Editor Activity

--- app/src/main/java/ir/mjavadf/ipponotes/NoteEditorActivity.java
+++ app/src/main/java/ir/mjavadf/ipponotes/NoteEditorActivity.java
@@ -1,14 +1,34 @@
 package ir.mjavadf.ipponotes;
 
+import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.Objects;
 
 public class NoteEditorActivity extends AppCompatActivity {
+
+  AppCompatTextView pageTitle;
+  MaterialButton cancelBTN,  saveBTN ;
+  MaterialEditText titleET,  noteET;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_note_editor);
-    getActionBar().hide();
+    Objects.requireNonNull(getActionBar()).hide();
+
+    init();
+  }
+
+  private void init() {
+    pageTitle   = findViewById(R.id.pageTitle);
+    cancelBTN   = findViewById(R.id.cancelBTN);
+    saveBTN     = findViewById(R.id.saveBTN);
+    titleET     = findViewById(R.id.titleET);
+    noteET      = findViewById(R.id.noteET);
   }
 }
