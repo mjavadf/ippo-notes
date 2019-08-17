9d8a7c9edcb121e3a5a09c5edde008be74b72064
mjavadf <javad.me1376@gmail.com>
2019-08-17 19:00:52 +0430

    Toggle mark on long click

--- app/src/main/java/ir/mjavadf/ipponotes/adapters/NotesAdapterCard.java
+++ app/src/main/java/ir/mjavadf/ipponotes/adapters/NotesAdapterCard.java
@@ -1,14 +1,16 @@
 package ir.mjavadf.ipponotes.adapters;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatTextView;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 
 import java.util.List;
@@ -42,6 +44,12 @@ public class NotesAdapterCard extends RecyclerView.Adapter<NotesAdapterCard.Card
   public void onBindViewHolder(@NonNull CardViewHolder holder, int i) {
     holder.title.setText(objects.get(i).getTitle());
     holder.note.setText(objects.get(i).getNote());
+
+    if (objects.get(i).getMark() == 1) {
+      holder.parent.setBackgroundColor(activity.getResources().getColor(R.color.marked_card));
+    } else {
+      holder.parent.setBackgroundColor(activity.getResources().getColor(R.color.unmarked_card));
+    }
   }
 
   @Override
@@ -49,11 +57,11 @@ public class NotesAdapterCard extends RecyclerView.Adapter<NotesAdapterCard.Card
     return objects.size();
   }
 
-  class CardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+  class CardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
     LinearLayout parent;
     AppCompatTextView title, note;
 
-    public CardViewHolder(@NonNull View itemView) {
+    CardViewHolder(@NonNull View itemView) {
       super(itemView);
 
       parent = itemView.findViewById(R.id.parent);
@@ -61,6 +69,7 @@ public class NotesAdapterCard extends RecyclerView.Adapter<NotesAdapterCard.Card
       note = itemView.findViewById(R.id.note);
 
       parent.setOnClickListener(this);
+      parent.setOnLongClickListener(this);
     }
 
     @Override
@@ -69,6 +78,61 @@ public class NotesAdapterCard extends RecyclerView.Adapter<NotesAdapterCard.Card
       Intent intent = new Intent(activity, ShowActivity.class);
       intent.putExtra(db.Notes.ID, object.getId());
       activity.startActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+      Note object = objects.get(getAdapterPosition());
+      if (object.getMark() == 0) {
+        object.setMark(1);
+
+        Animation anim = AnimationUtils.loadAnimation(activity, android.R.anim.fade_out);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+          @Override
+          public void onAnimationStart(Animation animation) {
+
+          }
+
+          @Override
+          public void onAnimationEnd(Animation animation) {
+            parent.setBackgroundColor(activity.getResources().getColor(R.color.marked_card));
+          }
+
+          @Override
+          public void onAnimationRepeat(Animation animation) {
+
+          }
+        });
+        parent.startAnimation(anim);
+
+        ContentValues values = new ContentValues();
+        values.put(db.Notes.MARK, object.getMark());
+        String[] whereArgs = {object.getId() + ""};
+        dbHelper.get().update(db.Tables.NOTES, values, db.Notes.ID + " = ? ", whereArgs);
+      } else {
+        object.setMark(0);
+
+        Animation anim = AnimationUtils.loadAnimation(activity, android.R.anim.fade_out);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+          @Override
+          public void onAnimationStart(Animation animation) { }
+
+          @Override
+          public void onAnimationEnd(Animation animation) {
+            parent.setBackgroundColor(activity.getResources().getColor(R.color.unmarked_card));
+          }
+
+          @Override
+          public void onAnimationRepeat(Animation animation) { }
+        });
+        parent.startAnimation(anim);
+
+        ContentValues values = new ContentValues();
+        values.put(db.Notes.MARK, object.getMark());
+        String[] whereArgs = {object.getId() + ""};
+        dbHelper.get().update(db.Tables.NOTES, values, db.Notes.ID + " = ? ", whereArgs);
+      }
+      return true;
     }
   }
 }
